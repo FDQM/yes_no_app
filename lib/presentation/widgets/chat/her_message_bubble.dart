9f9690210b1da43f14e2116e6c713be5267ee7fe
FDQM <franklindavid.quezadamartinez@gmail.com>
2024-04-29 13:05:57 -0400

    Yes No App

---
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class HerMessageBubble extends StatelessWidget {
+  final Message herMessage;
+  const HerMessageBubble({super.key, required this.herMessage});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              color: colors.secondary, borderRadius: BorderRadius.circular(20)),
+          child:  Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+             herMessage.text,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+         _ImageBubble(herImage: herMessage.imageUrl!,),
+        const SizedBox(
+          height: 10,
+        ),
+      ],
+    );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+  final String herImage;
+  const _ImageBubble({super.key, required this.herImage});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Image.network(
+         herImage,
+          width: size.width * 0.7,
+          height: 150,
+          fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) return child;
+
+            return Container(
+              width: size.width * 0.7,
+              height: 150,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: const Text('Mi Crush esta enviando una imagen'),
+            );
+          },
+        ));
+  }
+}
